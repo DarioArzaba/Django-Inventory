9c0e83340036bd605f565887ed35837047d5613b
Dario Arzaba <dario.arzaba@gmail.com>
2023-08-05 12:58:05 -0600

    feat: add frontend top navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,51 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+
+	<div id="wrapper">
+
+		<nav class="navbar is-dark">
+			<div class="container">
+				<div class="navbar-brand">
+
+					<router-link to="/" class="navbar-item brand-text"><strong>Tech Co</strong></router-link>
+					<a class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navMenu">
+                    	<span aria-hidden="true"></span>
+                    	<span aria-hidden="true"></span>
+                    	<span aria-hidden="true"></span>
+                	</a>
+            	</div>
+
+				<div id="navMenu" class="navbar-menu">
+
+					<div class="navbar-start">
+						<router-link to="/catalogue" class="navbar-item">Catalogue</router-link>
+						<router-link to="/orders" class="navbar-item">Orders</router-link>
+						<router-link to="/inventory" class="navbar-item">Inventory</router-link>
+						<router-link to="/kpi" class="navbar-item">KPI</router-link>
+            	    </div>
+					<div class="navbar-end">
+						<div class="navbar-item">
+							<div class="buttons">
+								<router-link to="/log-in" class="button is-light">Log In</router-link>
+								<router-link to="/cart" class="button is-success">
+									<span class="icon"><font-awesome-icon icon="fa-solid fa-shopping-cart"/></span>
+									<span>Cart</span>
+								</router-link>
+							</div>
+						</div>
+            	    </div>
+
+				</div>
+			</div>
+		</nav>
+
+		<div class="container">
+			<router-view/>
+		</div>
+
+	</div>
+
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import '../node_modules/bulma';
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,8 +5,9 @@ import store from './store'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faUserSecret)
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faShoppingCart)
 
 createApp(App).use(store).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
