197aee19cc731028d901c5d0c42b5995c7b91cda
Dario Arzaba <dario.arzaba@gmail.com>
2023-08-05 10:05:41 -0600

    feat: add fontawesome for vue component

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,4 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faUserSecret)
+
+createApp(App).use(store).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+	<p>ICON TEST <font-awesome-icon icon="fa-solid fa-user-secret" /></p>
   </div>
 </template>
