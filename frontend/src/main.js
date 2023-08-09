a381fe827d6b3f000cbe9c8ede84205d9ac5a06f
Dario Arzaba <dario.arzaba@gmail.com>
2023-08-08 21:06:41 -0600

    fix: fix cors allowd origin to display items

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
@@ -10,4 +13,4 @@ import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 library.add(faShoppingCart)
 
-createApp(App).use(store).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+createApp(App).use(store).use(router, axios).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
